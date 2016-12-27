ae00fff4551af08c6c5980a377103f4836596409
Enrique Escobar <enrique.escobar@axon-id.com>
2016-12-27 10:36:21 -0500

    --obdc-- x64

---
+++ Script-CreateOdbcDsn.ps1
@@ -0,0 +1,12 @@
+﻿Add-OdbcDsn -Name "AskidaOdbc64" 
+-DriverName "SQL Server" 
+-DsnType "System" 
+-Platform "64-bit" 
+-SetPropertyValue @("StatsLog_On=Yes"
+, "Description=Askida ODBC x64"
+, "Server=BUILD-DEV-HS"
+, "StatsLogFile=C:\Askida\ODBC\64\STATS.LOG"
+, "QueryLog_On=Yes"
+, "Language=us_english"
+, "QueryLogFile=C:\Askida\ODBC\64\QUERY.LOG")
+;
