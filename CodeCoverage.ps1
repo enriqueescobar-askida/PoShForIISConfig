139f96b062e72560b0ae5104e7738cf4c05025f2
Enrique Escobar <enrique.escobar@axon-id.com>
2017-04-04 13:05:17 -0400

    new stuff

---
+++ CodeCoverage.ps1
@@ -0,0 +1,18 @@
+﻿    $aFolder="C:\Askida\Build\Net\hydrosolution\api\0.2.0.107" ;
+    cd $aFolder ;
+    $openDir="CodeCoverage" ;
+    echo "mkdir $openDir" ;
+    mkdir $openDir;
+    $openCov="packages\OpenCover.4.6.519\tools\OpenCover.Console.exe" ;
+    $openUsr="user" ;
+    $openTgt="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe" ;
+    $openTar="BusinessLogic.Test\bin\Debug\BusinessLogic.Test.dll DataModels.Test\bin\Debug\DataModels.Test.dll API.Test\bin\Debug\API.Test.dll" ;
+    $openLog="trx" ;
+    $openDiag="$openDir\API.trx" ;
+    $openFilter="+[DataModels*]* +[API*]* +[BusinessLogic*]* -[BusinessLogic.Test]* -[API.Test]* -[DataModels.Test]*";
+    $openOut="$openDir\API.xml" ;
+    $openGenerator="packages\ReportGenerator.2.5.6\tools\ReportGenerator.exe" ;
+    $openTarDir="$openDir\Html" ;
+    $openLaunch="$openDir\Html\index.htm" ;
+    Invoke-Expression -Command 'cmd.exe /c $openCov -register:"$openUsr" -target:"$openTgt" -targetargs:"$openTar /logger:$openLog /Diag:$openDiag" -filter:$openFilter -mergebyhash -skipautoprops -output:"$openOut" '
+    Invoke-Expression -Command 'cmd.exe /c $openGenerator -reports:"$openOut" -targetdir:"$openTarDir" '
---
+++ Create-ReleaseDeploy.ps1
@@ -0,0 +1,45 @@
+﻿# This script will create a new release for a project
+## Octopus Server-URL
+$OctopusURL = "http://build-dev-hs.axon-id.loc:8080"
+## Octopus Server-APIKey-Jenkins
+$OctopusAPIKey = "API-QMSKCY7XZLXR7TTRUS7EQ7EKII"
+## HTTP Request Header
+$header = @{ "X-Octopus-ApiKey" = $OctopusAPIKey }
+## Environment
+$environmentName = "EnvHydroSoftIntegration"
+$environmentID = "Environments-6"
+$environJson = Invoke-WebRequest -Uri "$OctopusURL/api/Environments/all" -Headers $header| ConvertFrom-Json
+$environJson = $environJson | ?{$_.name -eq $environmentName}
+## Project
+$projectName = "ProjectHydroSoftBuildAPI"
+$projectJson = Invoke-WebRequest -Uri "$OctopusURL/api/projects/$projectName" -Headers $header| ConvertFrom-Json
+## Project template 
+$projectTmpl = Invoke-WebRequest -Uri "$OctopusURL/api/deploymentprocesses/deploymentprocess-$($projectJson.id)/template" -Headers $header | ConvertFrom-Json 
+## HTTP Request Body
+$body = @{
+  ProjectId = "Projects-3"
+  ChannelId = "Channels-5"
+  Version = "0.0.0.0"
+  ReleaseNotes = "the release notes " + $projectName
+}
+## try-catch
+try
+{
+  ## Release
+  $octoReleURL=$OctopusURL+"/api/releases?ignoreChannelRules=false"
+  $projReleAnsw = Invoke-WebRequest $octoReleURL -Method POST -Headers $header -Body ($body | ConvertTo-Json)
+  ## Deployment
+  $deployBody = @{ 
+                ReleaseID = ($projReleAnsw.Content|ConvertFrom-Json).Id
+                EnvironmentID = $environJson.Id
+  } | ConvertTo-Json
+  $deployAnsw = Invoke-WebRequest -Uri $OctopusURL/api/deployments -Method Post -Headers $header -Body $deployBody
+}
+catch
+{
+  $Result = $_.Exception.Response.GetResponseStream()
+  $Reader = New-Object System.IO.StreamReader($result)
+  $ResponseBody = $Reader.ReadToEnd();
+  $Response = $ResponseBody | ConvertFrom-Json
+  $Response.Errors
+}
